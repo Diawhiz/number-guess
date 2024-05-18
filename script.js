8cc063a08cab5fa26d614a08b6c6dd59bd433ff4
Ariyo Israel Gbemisola <igbemisola53@gmail.com>
2024-05-18 21:02:00 +0100

    Added script file to the web app to program the game

---
+++ script.js
@@ -0,0 +1,8 @@
+const numberGuess = document.getElementById('input-box').value;
+var Warning = document.getElementById('warning');
+var guessedNumber = document.getElementById('guessed-number');
+var guessResult = document.getElementById('guess-result');
+
+function myGuess() {
+    
+}
