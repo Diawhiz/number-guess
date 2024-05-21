d77e2220474f33fc1c066155a22abec3396dc810
Ariyo Israel Gbemisola <igbemisola53@gmail.com>
2024-05-21 04:21:30 +0100

    Used chatgpt to make the code look better

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
-var Warning = document.getElementById('warning');
-var guessedNumber = document.getElementById('guessed-number');
-var guessResult = document.getElementById('guess-result');
+const warning = document.getElementById('warning');
+const guessedNumber = document.getElementById('guessed-number');
+const guessResult = document.getElementById('guess-result');
 
-
-function Guessed() {
+function guessed() {
     const randomNumber = Math.round(Math.random() * 20);
     const numberGuess = document.getElementById('input-box').value;
-    const guess = parseInt(numberGuess)
+    const guess = parseInt(numberGuess);
 
-     if (numberGuess.length == 0 || numberGuess.length == "") {
-        Warning.innerHTML = 'Please enter a number to guess';
+    if (numberGuess.length === 0) {
+        warning.innerHTML = 'Please enter a number to guess';
         document.getElementById('input-box').style.border = '1px solid red';
         document.getElementById('submit').style.border = '1px solid red';
 
         setTimeout(() => {
-            document.getElementById('input-box').style.border = '1px solid #fff';
-            document.getElementById('submit').style.border = '1px solid #fff';
-            document.getElementById('warning').style.display = none;
+            document.getElementById('input-box').style.border = '';
+            document.getElementById('submit').style.border = '';
+            warning.style.display = 'none';
         }, 3000);
+        warning.style.display = 'block';
         return false;
     }
 
-    if (guess >= 21) {
-        Warning.innerHTML = 'You can only guess between 0 and 20'
+    if (guess < 0 || guess > 20) {
+        warning.innerHTML = 'You can only guess between 0 and 20';
+        warning.style.display = 'block';
         return false;
     }
+
+    warning.style.display = 'none';
 
     if (guess === randomNumber) {
         guessedNumber.innerHTML = randomNumber;
-        guessResult.innerHTML = `${guess} You guessed right`;
-        return false;
+        guessResult.innerHTML = `${guess} - You guessed right!`;
     } else {
         guessedNumber.innerHTML = randomNumber;
-        guessResult.innerHTML = `${guess} You guessed wrong!`;
-        return false;
+        guessResult.innerHTML = `${guess} - You guessed wrong!`;
     }
 
+    return false;
 }
-
-
-
